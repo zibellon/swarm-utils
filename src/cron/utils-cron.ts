61406d28fd78adf5bda6d55f2cdad2e8e3968edd
zibellon <artemi199800@gmail.com>
2024-08-30 11:28:07 +0400

    add logs to cron

--- src/cron/utils-cron.ts
+++ src/cron/utils-cron.ts
@@ -1,6 +1,6 @@
 import { CronJob } from 'cron';
 import { getProcessEnv } from 'src/utils/utils-env-config';
-import { logError } from 'src/utils/utils-logger';
+import { logError, logInfo } from 'src/utils/utils-logger';
 import { cronBackupServiceList } from './utils-cron-backup-service';
 import { cronCleanNodeList } from './utils-cron-clean-node';
 import { cronCleanServiceList } from './utils-cron-clean-service';
@@ -8,8 +8,19 @@ import { cronCleanServiceList } from './utils-cron-clean-service';
 let isCronProgress = false;
 
 export async function initCron() {
+  logInfo('initCron', {
+    expr: getProcessEnv().SWARM_UTILS_CRON_EXPR,
+    isCronCleanNode: getProcessEnv().SWARM_UTILS_IS_CRON_CLEAN_NODE,
+    isCronCleanService: getProcessEnv().SWARM_UTILS_IS_CRON_CLEAN_SERVICE,
+    isCronBackupService: getProcessEnv().SWARM_UTILS_IS_CRON_BACKUP_SERVICE,
+  });
+
   const cronJob = new CronJob(getProcessEnv().SWARM_UTILS_CRON_EXPR, async () => {
     const dateCron = new Date();
+    logInfo('cronTick.INIT', {
+      dateCron,
+      isCronProgress,
+    });
 
     if (isCronProgress === false) {
       isCronProgress = true;
