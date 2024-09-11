69930985228b49a5dfb05e1de849cd6bf0e29c18
zibellon <artemi199800@gmail.com>
2024-09-11 16:29:43 +0400

    fix names for all help-service and UPD examples

--- src/utils/utils-names.ts
+++ src/utils/utils-names.ts
@@ -1,9 +1,9 @@
-const prefix = 'swarm-utils';
-const prefix_lock = 'swarm-utils-lock';
+const prefix = 'swarm_utils';
+const prefix_lock = 'swarm_utils_lock';
 
 // service.name / node.id_node.name
 export function nameLock(key: string) {
-  return `${prefix_lock}_${key}`;
+  return `${prefix_lock}-${key}`;
 }
 
 //---------
@@ -12,13 +12,13 @@ export function nameLock(key: string) {
 
 // node.id_node.name
 export function nameCleanNodeImages(key: string) {
-  return `${prefix}_clean-node_${key}_images`;
+  return `${prefix}-clean_node-${key}-images`;
 }
 export function nameCleanNodeBuilder(key: string) {
-  return `${prefix}_clean-node_${key}_builder`;
+  return `${prefix}-clean_node-${key}-builder`;
 }
 export function nameCleanNodeContainers(key: string) {
-  return `${prefix}_clean-node_${key}_containers`;
+  return `${prefix}-clean_node-${key}-containers`;
 }
 
 // node.id_node.name
@@ -32,26 +32,26 @@ export function nameGetAllServiceNamesForNode(key: string) {
 
 // service.name
 export function nameCleanServiceExec(key: string) {
-  return `${prefix}_clean-service_${key}_exec`;
+  return `${prefix}-clean_service-${key}-exec`;
 }
 
 // service.name
 export function nameBackupServiceExec(key: string) {
-  return `${prefix}_backup-service_${key}_exec`;
+  return `${prefix}-backup_service-${key}-exec`;
 }
 export function nameBackupServiceScaleDown(key: string) {
-  return `${prefix}_backup-service_${key}_scale-down`;
+  return `${prefix}-backup_service-${key}-scale_down`;
 }
 export function nameBackupServiceTarUpload(key: string) {
-  return `${prefix}_backup-service_${key}_tar-upload`;
+  return `${prefix}-backup_service-${key}-tar_upload`;
 }
 export function nameBackupServiceScaleUp(key: string) {
-  return `${prefix}_backup-service_${key}_scale-up`;
+  return `${prefix}-backup_service-${key}-scale_up`;
 }
 
 // service.name
 export function nameUpdateService(key: string) {
-  return `${prefix}_update-service_${key}`;
+  return `${prefix}-update_service-${key}`;
 }
 
 // service.name
